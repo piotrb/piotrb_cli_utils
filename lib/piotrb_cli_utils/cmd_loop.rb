c2f722d013e5c77d28db0095910a306d58aab6e2
Piotr Banasik <piotr@jane.app>
2025-04-01 15:16:48 -0700

    Support proc prompt parameter

--- lib/piotrb_cli_utils/cmd_loop.rb
+++ lib/piotrb_cli_utils/cmd_loop.rb
@@ -2,6 +2,9 @@
 
 module PiotrbCliUtils
   module CmdLoop
+    # @param prompt [String, Proc] The prompt to use for the command loop.
+    #   If a proc is given, it will be called for each time the prompt is
+    #   displayed.
     def run_cmd_loop(prompt = '=> ')
       reader = TTY::Reader.new(interrupt: :noop)
       reader.on(:keyctrl_c, :keyescape) do
@@ -14,7 +17,8 @@ module PiotrbCliUtils
 
       catch(:stop) do
         loop do
-          line = reader.read_line(prompt)
+          actual_prompt = prompt.respond_to?(:call) ? prompt.call : prompt
+          line = reader.read_line(actual_prompt)
           line.strip!
           yield(line)
         end
